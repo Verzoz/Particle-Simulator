7f6bc9f2974ce4cd7cd1b098d0254ad285cbbbb3
Verzoz <ipad.daniele25@gmail.com>
2026-07-08 15:38:52 +0200

    Modificata la simulazione per simulare una scala reale: aggiunto il parallelismo tra pixel/frame a metri/secondo

--- Particle Simulator/main.cpp
+++ Particle Simulator/main.cpp
@@ -14,22 +14,40 @@ int main()
     window.setVerticalSyncEnabled(true); // VSync attivato
     // window.setFramerateLimit(60);
 
+    // Creamo la view della simulazione in 4k
+    sf::View Simulazione(sf::FloatRect({ 0.0f, 0.0f }, { 3840.0f, 2160.0f } ));
+
+
+    // Impostiamo la window che "Guarda" la view fisica appena creata
+    window.setView(Simulazione);
+
+
+
+    // VARIABILI GENERICHE
+    float gravita = 9.81f;
+    float energyDissipationFactor = 0.1f;
+
+    // Pixel per Metro: serve per convertire le misure della simulazione in metri/secondo invece che pixel/secondo
+    float ppm = 3180.0f; // 1 metro = 3180 px
+
+    sf::Vector2f areaSimulazione(3840.0f, 2160.0f);
+
+    // Orologio per rendere tutto indipendente dal framerate
+    sf::Clock orologio;
+
 
 
     // Creo una particella rossa
-    sf::CircleShape particella(20.0f);
+    sf::CircleShape particella(0.002f * ppm); // 0.002 metri di raggio, cioè 2 mm
     particella.setFillColor(sf::Color(46, 129, 246));
-
-    
-    sf::Vector2f pos = particella.getPosition();
-    sf::Vector2u dimFinestra = window.getSize();
+       
 
 
     // Posizione iniziale della Particella
-    particella.setPosition({ ((float)dimFinestra.x / 2) - (particella.getRadius())  ,   ((float)dimFinestra.y / 2) - (particella.getRadius()) });
+    particella.setPosition({ (areaSimulazione.x / 2) - (particella.getRadius())  ,   (areaSimulazione.y / 2) - (particella.getRadius()) });
 
     // Velocità iniziale della particella
-    sf::Vector2f velocita(22.0f, 22.0f);
+    sf::Vector2f velocita(0.0f, 0.0f);
 
 
 
@@ -39,6 +57,11 @@ int main()
     // Run loop
     while (window.isOpen())
     {
+
+        // Misuriamo il tempo tra un frame e l altro e lo salviamo in dt per rendere tutta la fisica indipendente dal framerate
+        float dt = orologio.restart().asSeconds();
+
+
         // Checks all the events triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
@@ -54,9 +77,12 @@ int main()
 
         //UPDATE
 
+        // Facciamo agire la gravità
+        velocita.y += gravita * dt;
+
         
         // Facciamo muovere la particella
-        particella.move(velocita);
+        particella.move(velocita * dt * ppm);
 
         // Leggiamo dove si trova la particella
         sf::Vector2f pos = particella.getPosition();
@@ -69,25 +95,25 @@ int main()
         if (pos.x < 0.0f) // Muro Sinistro
         {
             particella.setPosition({ 0.0f, pos.y });
-            velocita.x = -velocita.x;
+            velocita.x = - ( velocita.x * ( 1.0f - energyDissipationFactor) ) ;
         }
 
-        if (pos.x > (float)dimFinestra.x - (particella.getRadius() * 2.0f)) // Muro Destro
+        if (pos.x > areaSimulazione.x - (particella.getRadius() * 2.0f)) // Muro Destro
         {
-            particella.setPosition({ (float)dimFinestra.x - (particella.getRadius() * 2.0f), pos.y });
-            velocita.x = -velocita.x;
+            particella.setPosition({ areaSimulazione.x - (particella.getRadius() * 2.0f), pos.y });
+            velocita.x = - ( velocita.x * ( 1.0f - energyDissipationFactor) );
         }
 
         if (pos.y < 0.0f) // Muro Sopra
         {
             particella.setPosition({ pos.x, 0.0f });
-            velocita.y = -velocita.y;
+            velocita.y = - ( velocita.y * ( 1.0f - energyDissipationFactor ) );
         }
 
-        if (pos.y > (float)dimFinestra.y - (particella.getRadius() * 2.0f)) // Muro Sotto
+        if (pos.y > areaSimulazione.y - (particella.getRadius() * 2.0f)) // Muro Sotto
         {
-            particella.setPosition({ pos.x, (float)dimFinestra.y - (particella.getRadius() * 2.0f) });
-            velocita.y = -velocita.y;
+            particella.setPosition({ pos.x, areaSimulazione.y - (particella.getRadius() * 2.0f) });
+            velocita.y = - ( velocita.y * ( 1.0f - energyDissipationFactor ) );
         }
         
         
