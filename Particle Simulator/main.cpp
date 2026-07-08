5a62bbdda5df51b411ff48544933129312208206
Verzoz <ipad.daniele25@gmail.com>
2026-07-08 14:55:37 +0200

    Aggiunta la collisione fisica tra la particella e i bordi dello schermo

--- Particle Simulator/main.cpp
+++ Particle Simulator/main.cpp
@@ -10,9 +10,30 @@
 int main()
 {
 
-    sf::RenderWindow window(sf::VideoMode({1270, 720}), "Prima App");
+    sf::RenderWindow window(sf::VideoMode({1280, 720}), "Prima App");
     window.setVerticalSyncEnabled(true); // VSync attivato
     // window.setFramerateLimit(60);
+
+
+
+    // Creo una particella rossa
+    sf::CircleShape particella(20.0f);
+    particella.setFillColor(sf::Color(46, 129, 246));
+
+    
+    sf::Vector2f pos = particella.getPosition();
+    sf::Vector2u dimFinestra = window.getSize();
+
+
+    // Posizione iniziale della Particella
+    particella.setPosition({ ((float)dimFinestra.x / 2) - (particella.getRadius())  ,   ((float)dimFinestra.y / 2) - (particella.getRadius()) });
+
+    // Velocità iniziale della particella
+    sf::Vector2f velocita(22.0f, 22.0f);
+
+
+
+
 
 
     // Run loop
@@ -33,6 +54,43 @@ int main()
 
         //UPDATE
 
+        
+        // Facciamo muovere la particella
+        particella.move(velocita);
+
+        // Leggiamo dove si trova la particella
+        sf::Vector2f pos = particella.getPosition();
+
+
+
+        // Logica per la collisione con i bordi dello schermo
+
+        
+        if (pos.x < 0.0f) // Muro Sinistro
+        {
+            particella.setPosition({ 0.0f, pos.y });
+            velocita.x = -velocita.x;
+        }
+
+        if (pos.x > (float)dimFinestra.x - (particella.getRadius() * 2.0f)) // Muro Destro
+        {
+            particella.setPosition({ (float)dimFinestra.x - (particella.getRadius() * 2.0f), pos.y });
+            velocita.x = -velocita.x;
+        }
+
+        if (pos.y < 0.0f) // Muro Sopra
+        {
+            particella.setPosition({ pos.x, 0.0f });
+            velocita.y = -velocita.y;
+        }
+
+        if (pos.y > (float)dimFinestra.y - (particella.getRadius() * 2.0f)) // Muro Sotto
+        {
+            particella.setPosition({ pos.x, (float)dimFinestra.y - (particella.getRadius() * 2.0f) });
+            velocita.y = -velocita.y;
+        }
+        
+        
 
 
 
@@ -44,6 +102,13 @@ int main()
         window.clear(sf::Color::Black);
 
         // Draw things for the current frame in between 'clear' & 'display'
+
+
+        // renderizziamo la particella
+        window.draw(particella);
+
+
+
 
         window.display();
     }
